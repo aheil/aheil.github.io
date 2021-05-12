f9c3edad9ce1bab57adf7e9744528fc0a7dca602
Andreas Heil <info@aheil.de>
2021-05-12 23:45:12 +0200

    remark42 included in mode switcher

--- assets/js/mode-switcher.js
+++ assets/js/mode-switcher.js
@@ -2,15 +2,15 @@ let systemInitiatedDark = window.matchMedia("(prefers-color-scheme: dark)");
 let theme = sessionStorage.getItem('theme');
 
 function prefersColorTest(systemInitiatedDark) {
-  if (systemInitiatedDark.matches) {
-	  document.documentElement.setAttribute('data-theme', 'dark');
-	   // document.getElementById("theme-toggle").innerHTML = "Light Mode";
-   	sessionStorage.setItem('theme', '');
-  } else {
-	  document.documentElement.setAttribute('data-theme', 'light');
-    // document.getElementById("theme-toggle").innerHTML = "Dark Mode";
-    sessionStorage.setItem('theme', '');
-  }
+	if (systemInitiatedDark.matches) {
+		document.documentElement.setAttribute('data-theme', 'dark');
+		// document.getElementById("theme-toggle").innerHTML = "Light Mode";
+		sessionStorage.setItem('theme', '');
+	} else {
+		document.documentElement.setAttribute('data-theme', 'light');
+		// document.getElementById("theme-toggle").innerHTML = "Dark Mode";
+		sessionStorage.setItem('theme', '');
+	}
 }
 systemInitiatedDark.addListener(prefersColorTest);
 
@@ -20,20 +20,24 @@ function modeSwitcher() {
 	if (theme === "dark") {
 		document.documentElement.setAttribute('data-theme', 'light');
 		sessionStorage.setItem('theme', 'light');
+		window.REMARK42.changeTheme('light');
 		// document.getElementById("theme-toggle").innerHTML = "Dark Mode";
-	}	else if (theme === "light") {
+	} else if (theme === "light") {
 		document.documentElement.setAttribute('data-theme', 'dark');
 		sessionStorage.setItem('theme', 'dark');
+		window.REMARK42.changeTheme('dark');
 		// document.getElementById("theme-toggle").innerHTML = "Light Mode";
 	} else if (systemInitiatedDark.matches) {
 		document.documentElement.setAttribute('data-theme', 'light');
 		sessionStorage.setItem('theme', 'light');
+		window.REMARK42.changeTheme('light');
 		//let theme = sessionStorage.getItem('theme');
 		//console.log("this was triggered");
 		// document.getElementById("theme-toggle").innerHTML = "Dark Mode";
 	} else {
 		document.documentElement.setAttribute('data-theme', 'dark');
 		sessionStorage.setItem('theme', 'dark');
+		window.REMARK42.changeTheme('dark');
 		// document.getElementById("theme-toggle").innerHTML = "Light Mode";
 	}
 }
@@ -43,9 +47,11 @@ if (theme === "dark") {
 	sessionStorage.setItem('theme', 'dark');
 	document.getElementById("theme-toggle").checked = false;
 	// document.getElementById("theme-toggle").innerHTML = "Light Mode";
+	window.REMARK42.changeTheme('dark');
 } else if (theme === "light") {
 	document.documentElement.setAttribute('data-theme', 'light');
 	sessionStorage.setItem('theme', 'light');
 	document.getElementById("theme-toggle").checked = true;
 	// document.getElementById("theme-toggle").innerHTML = "Dark Mode";
+	window.REMARK42.changeTheme('light');
 }
