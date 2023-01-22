6c502c9544f7ed28e46480837df491a16d3d9e43
Andreas Heil <info@aheil.de>
2023-01-22 23:31:04 +0100

    js updated

--- assets/incidents.js
+++ assets/incidents.js
@@ -2,17 +2,10 @@ fetch("../assets/incidents.json")
 .then(response => {
    return response.json();
 })
-.then(jsondata => doRun(jsondata));
-
-
-function doRun(incidents)
-{
-    incidents.foreach(addIncident);
-}
+.then(jsondata => jsondata.forEach(addIncident));
 
 function addIncident(incident)
 {
-
     var marker = L.marker([incident.lat, incident.long]).addTo(map);
     marker.bindPopup("<b>" + incident.name +"</b><br>" + incident.date + "<br>" + incident.link);
 }
