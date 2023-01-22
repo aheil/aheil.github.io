9dccf76a53180ddae5191d463e9cea8c8b9e118e
Andreas Heil <info@aheil.de>
2023-01-22 22:57:27 +0100

    minor changes

--- assets/incidents.js
+++ assets/incidents.js
@@ -1,2 +1,5 @@
-var incidents = require('../assets/incidents.json');
-console.log(incidents);
+fetch("../assets/incidents.json")
+.then(response => {
+   return response.json();
+})
+.then(jsondata => console.log(jsondata));
