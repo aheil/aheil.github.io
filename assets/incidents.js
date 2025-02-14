9dd217477250094c2e6ae5e4aac6335e824b2d92
Andreas Heil <andreas.heil@hs-heilbronn.de>
2025-02-14 09:51:38 +0100

    incident added

--- assets/incidents.js
+++ assets/incidents.js
@@ -1,13 +1,18 @@
 
 var incidents;
 
-function addIncident(incident, popup) {
+function addIncident(incident, popup)
+{
 
   var opacity;
 
-  switch (incident.year) {
-    case "2024":
+  switch (incident.year)
+  {
+    case "2025":
       opacity = 1.0;
+      break;
+    case "2024":
+      opacity = 0.9;
       break;
     case "2023":
       opacity = 0.8;
@@ -16,10 +21,10 @@ function addIncident(incident, popup) {
       opacity = 0.7;
       break;
     case "2021":
-      opacity = 0.5;
+      opacity = 0.6;
       break;
     default:
-      opacity = 0.4;
+      opacity = 0.5;
   }
 
   var marker = L.marker([incident.lat, incident.long]).addTo(map);
@@ -29,12 +34,15 @@ function addIncident(incident, popup) {
 }
 
 fetch("../assets/incidents.json")
-  .then(function (response) {
+  .then(function (response)
+  {
     return response.json();
   })
-  .then(function (data) {
+  .then(function (data)
+  {
     incidents = data.incidents;
-    for (let i = 0; i < incidents.length; i++) {
+    for (let i = 0; i < incidents.length; i++)
+    {
       var popup = i == (incidents.length - 1);
       addIncident(incidents[i], popup);
     }
